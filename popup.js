021060af8be2720b710dc3e1a2b4896a08b5d0b2
Bhavik Thakkar <bhavikthakkar@Bhaviks-MacBook-Pro.local>
2019-03-11 17:15:49 -0500

    Initial Commit with basic design and functionalities

---
+++ popup.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('clickIt');
+    var anaplanAccount = document.getElementById('clickIt2');
+    //var imgURL = chrome.extesnion.getURL("anaplan.png");
+    //document.getElementById("clickIt").src = imgURL
+    //checkPageButton.addEventListener('click', function() {
+
+      //chrome.tabs.getSelected(null, function(tab) {
+      //  alert("Select your option");
+      //});
+    //}, false);
+  }, false);
+
+
+
+
+  //var imgURL = chrome.extension.getURL("images/myimage.png");
+  //document.getElementById("someImage").src = imgURL;
